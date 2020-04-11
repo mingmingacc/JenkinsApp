37934e1cf77426739c9c59c98560d5ec63ce14f7
mingmingacc <857122881@qq.com>
2020-04-11 13:42:13 +0800

    添加一条注释

---
+++ app/src/main/java/com/example/jenkinsapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.jenkinsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //这是一条注释
+    }
+}
